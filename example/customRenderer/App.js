701821d983f8523802feb4baba4419614c4f8aac
4noth1ng <1766695958@qq.com>
2023-03-24 14:52:27 +0800

    canvas渲染demo

---
+++ example/customRenderer/App.js
@@ -0,0 +1,14 @@
+import { h } from "../../lib/my-mini-vue.esm.js";
+
+export const App = {
+  render() {
+    return h("rect", { x: this.x, y: this.y });
+  },
+
+  setup() {
+    return {
+      x: 100,
+      y: 100,
+    };
+  },
+};
