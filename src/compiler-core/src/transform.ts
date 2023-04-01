295e8f25ed2dba45cc0273dfa181013324e1e3d3
4noth1ng <1766695958@qq.com>
2023-04-01 21:53:23 +0800

    代码生成string类型

--- src/compiler-core/src/transform.ts
+++ src/compiler-core/src/transform.ts
@@ -1,9 +1,15 @@
 import { NodeTypes } from "./ast";
 
-export function transform(root, options) {
+export function transform(root, options = {}) {
   const context = createTransformContext(root, options);
   // 1. 遍历 - 深度优先搜索
   traverseNode(root, context);
+
+  createRootCodegen(root);
+}
+
+function createRootCodegen(root: any) {
+  root.codegenNode = root.children[0];
 }
 
 function createTransformContext(root, options) {
---
+++ src/compiler-core/tests/codegen.spec.ts
@@ -0,0 +1,14 @@
+import { generate } from "../src/codegen";
+import { baseParse } from "../src/parse";
+import { transform } from "../src/transform";
+
+describe("codegen", () => {
+  it("string", () => {
+    const ast = baseParse("hi");
+    transform(ast);
+    const { code } = generate(ast);
+
+    // snapshot - 生成对照组
+    expect(code).toMatchSnapshot();
+  });
+});
